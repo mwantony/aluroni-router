e25047d43be76837dd923bcbb29003c317ba6ffd
Antony Maziero Warken <againplayi7@gmail.com>
2022-05-27 13:04:45 -0300

    push

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import PaginaPadrao from 'components/PaginaPadrao';
 import Cardapio from 'pages/Cardapio';
 import Inicio from 'pages/Inicio';
 import NotFound from 'pages/NotFound';
+import Prato from 'pages/Prato';
 import Sobre from 'pages/Sobre';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 export default function AppRouter() {
@@ -19,6 +20,7 @@ export default function AppRouter() {
             <Route path="sobre" element={<Sobre></Sobre>}></Route>
           </Route>
           <Route path="*" element={<NotFound></NotFound>}></Route>
+          <Route path="prato/:id" element={<Prato></Prato>}></Route>
         </Routes>
         <Footer/>
       </Router>
