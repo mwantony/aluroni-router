4e585a70ed4b2a0f21f8ddbd5d2232be22fc6ca5
Antony Maziero Warken <againplayi7@gmail.com>
2022-05-27 16:27:02 -0300

    react router cap 6 vid 1 finalizado

--- src/pages/Prato/index.tsx
+++ src/pages/Prato/index.tsx
@@ -2,13 +2,17 @@ import styles from './Prato.module.scss';
 import { useNavigate, useParams } from 'react-router-dom';
 import cardapio from 'data/cardapio.json';
 import TagsPrato from 'components/TagsPrato';
+import NotFound from 'pages/NotFound';
 
 export default function Prato() {
   const navigate = useNavigate();
   const { id } = useParams();
   const prato = cardapio.find((item) => item.id === Number(id));
-  if (!prato) return '';
-
+  if (!prato) {
+    return <NotFound></NotFound>;
+  
+  }
+  
   return (
     <>
       <button
